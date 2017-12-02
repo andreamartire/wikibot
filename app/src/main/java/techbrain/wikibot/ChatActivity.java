cbadc0338085c746cdf090d155a43b6a10db964b
andreamartire@gmail.com <andreamartire@gmail.com>
2017-12-02 14:07:47 +0100

    info

--- app/src/main/java/techbrain/wikibot/ChatActivity.java
+++ app/src/main/java/techbrain/wikibot/ChatActivity.java
@@ -1,9 +1,13 @@
 package techbrain.wikibot;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,20 +39,58 @@ public class ChatActivity extends Activity {
                 listItems);
         list.setAdapter(adapter);
 
+        final Context context = this;
 
         final EditText ediBox = (EditText) findViewById(R.id.editBox);
+        ediBox.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    manageMessage(context,  ediBox);
+                }
+                return false;
+            }
+        });
+        ediBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    manageMessage(context,  ediBox);
+                }
+                return false;
+            }
+        });
 
         Button sendButton = (Button) findViewById(R.id.sendButton);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //update list
-                listItems.add(ediBox.getText().toString());
-                //clear edit box
-                ediBox.setText("");
-                adapter.notifyDataSetChanged();
+                manageMessage(context, ediBox);
             }
         });
+    }
+
+    public void manageMessage(Context context, EditText editBox){
+        String message = editBox.getText().toString();
+
+        if(!message.isEmpty()){
+            //update list
+            listItems.add(message);
+            //clear edit box
+            editBox.setText("");
+
+            //hide keyboard
+            InputMethodManager inputManager =
+                    (InputMethodManager) context.
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(
+                    editBox.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+
+            adapter.notifyDataSetChanged();
+        }
     }
 
     @Override
