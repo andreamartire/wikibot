3b0985619679fe9b852287d42fdd9f5eeba1e907
andreamartire@gmail.com <andreamartire@gmail.com>
2017-12-17 22:43:49 +0100

    improvements

--- app/src/main/java/techbrain/wikibot/delegates/WikiCommons.java
+++ app/src/main/java/techbrain/wikibot/delegates/WikiCommons.java
@@ -44,7 +44,9 @@ public class WikiCommons {
 						if(elements != null){
 							int size = elements.length;
 							for(String element : elements){
-								System.out.println(element);
+								String[] sections = element.split("\" class=\"image\"");
+								String[] links = sections[0].split("><a href=\"");
+								System.out.println(links[1]);
 							}
 						}
 					}
